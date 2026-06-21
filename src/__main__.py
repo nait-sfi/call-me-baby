13b768c161f691b1fa7403e9bc73dd0940a9839c
nait-sfi <norou6@gmail.com>
2026-06-21 15:17:02 +0100

    add: read parse and validate the function definition file

--- src/__main__.py
+++ src/__main__.py
@@ -1,11 +1,32 @@
-from llm_sdk import Small_LLM_Model
+# from llm_sdk import Small_LLM_Model
+import argparse
+import json
+from src.schema import FunctionDef
 
 
 def main() -> None:
-    model = Small_LLM_Model()
+    arg_parser = argparse.ArgumentParser(description="call me baby")
+    arg_parser.add_argument("--functions_definition", required=True)
+    arg_parser.add_argument(
+        "--input",
+        default="data/input/function_calling_tests.json"
+        )
+    arg_parser.add_argument(
+        "--output",
+        default="data/output/function_calls.json"
+        )
 
-    print("Model loaded successfully!")
-    print("Tokens for 'Hello World':", model.encode("Hello World"))
+    args = arg_parser.parse_args()
+    fn_df_file = args.functions_definition
+
+    with open(fn_df_file, "r") as file:
+        raw_data = json.load(file)
+        print(raw_data)
+        validated_functions = []
+        validated_functions = [FunctionDef.model_validate(item)
+                               for item in raw_data
+                               ]
+        print(validated_functions)
 
 
 if __name__ == "__main__":
